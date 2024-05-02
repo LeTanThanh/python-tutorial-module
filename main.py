e046138732de3d4c3adf5e80013accbe03d64330
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-02 23:42:54 +0700

    Importing module objects

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
 if __name__ == "__main__":
-  print("Python Modules")
+  # Importing module objects
+
+  import pricing
+  import pricing as selling_price
+  from pricing import get_net_price
+  from pricing import get_net_price as calculate_net_price
+
+  net_price = pricing.get_net_price(
+    price = 100,
+    tax_rate = 0.01
+  )
+  print(net_price)
+
+  net_price = selling_price.get_net_price(
+    price = 100,
+    tax_rate = 0.01
+  )
+  print(net_price)
+
+  net_price = get_net_price(
+    price = 100,
+    tax_rate = 0.01
+  )
+  print(net_price)
+
+  net_price = calculate_net_price(
+    price = 100,
+    tax_rate = 0.01
+  )
+  print(net_price)
